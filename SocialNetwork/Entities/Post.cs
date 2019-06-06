54aea45e5069084bbcb5761b853551b01fb19aa6
Leonardo Sousa <leorick97@gmail.com>
2019-06-05 22:01:41 -0300

    Implementation of Output Console with method StringBuilder

--- SocialNetwork/Entities/Post.cs
+++ SocialNetwork/Entities/Post.cs
@@ -32,5 +32,21 @@ namespace SocialNetwork.Entities
         {
             Comments.Remove(comment);
         }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine(Title);
+            sb.Append(Likes + " Likes ");
+            sb.AppendLine(Moment.ToString("dd/MM/yyyy HH:mm:ss"));
+            sb.AppendLine(Content);
+            sb.AppendLine("Comments: ");
+            foreach(Comment c in Comments)
+            {
+                sb.AppendLine(" - " + c.Text);
+            }
+
+            return sb.ToString();
+        }
     }
 }
--- SocialNetwork/Program.cs
+++ SocialNetwork/Program.cs
@@ -7,8 +7,6 @@ namespace SocialNetwork
     {
         static void Main(string[] args)
         {
-            Comment c1 = new Comment("Have a nice trip");
-            Comment c2 = new Comment("Wow that's awesome!");
 
             Post p1 = new Post(
                 DateTime.Parse("21/06/2018 13:05:44"),
@@ -16,6 +14,27 @@ namespace SocialNetwork
                 "I'm going to visit this wonderful country!",
                 12
                 );
+
+
+            Comment c1 = new Comment("Have a nice trip");
+            p1.AddComment(c1);
+            Comment c2 = new Comment("Wow that's awesome!");
+            p1.AddComment(c2);
+
+            Post p2 = new Post(
+                DateTime.Parse("28/07/2018 23:14:19"),
+                "Good night guys",
+                "See you tomorrow", 
+                5);
+
+            Comment c3 = new Comment("Good night");
+            p2.AddComment(c3);
+            Comment c4 = new Comment("May the force be with you");
+            p2.AddComment(c4);
+
+            Console.WriteLine(p1);
+            Console.WriteLine();
+            Console.WriteLine(p2);
         }
     }
 }
